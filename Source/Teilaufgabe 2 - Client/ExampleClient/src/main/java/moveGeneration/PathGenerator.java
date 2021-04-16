a92b57384407b1c687a012f6e5bfe9ae61411a2d
Michal Robert Žák <a11922222@unet.univie.ac.at>
2021-04-16 11:28:07 +0200

    Started development of dijkstra. The algorithm is still untested but should be able to find the shortest distance to a specific node. It still missing the path to the node though.

--- Source/Teilaufgabe 2 - Client/ExampleClient/src/main/java/moveGeneration/PathGenerator.java
+++ Source/Teilaufgabe 2 - Client/ExampleClient/src/main/java/moveGeneration/PathGenerator.java
@@ -1,10 +1,14 @@
 package moveGeneration;
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import mapHelpers.EGameEntity;
 import mapHelpers.Position;
 import moveHelpers.EMove;
 
@@ -23,8 +27,54 @@ public class PathGenerator {
 		this.fma = fma;
 	}
 
+	// TODO: Refactor this class
 	public List<EMove> getPathTo(Position pos) {
-		// TODO: DIJKSTRA!
+		Position current = fma.getEntityPosition(EGameEntity.MYPLAYER);
+
+		HashMap<Position, Integer> cost = new HashMap<>();
+
+		Set<Position> neighbours = new HashSet<>();
+		Set<Position> visited = new HashSet<>();
+
+		cost.put(current, 0);
+
+		while (!visited.contains(pos)) {
+			visited.add(current);
+
+			// up
+			if (current.gety() > 0 && !visited.contains(new Position(current.getx(), current.gety() - 1)))
+				neighbours.add(new Position(current.getx(), current.gety() - 1));
+
+			// dowm
+			if (current.gety() < fma.getHeight() && !visited.contains(new Position(current.getx(), current.gety() + 1)))
+				neighbours.add(new Position(current.getx(), current.gety() + 1));
+
+			// right
+			if (current.getx() < fma.getWidth() && !visited.contains(new Position(current.getx() + 1, current.gety())))
+				neighbours.add(new Position(current.getx() + 1, current.gety()));
+
+			// left
+			if (current.getx() > 0 && !visited.contains(new Position(current.getx() - 1, current.gety())))
+				neighbours.add(new Position(current.getx() - 1, current.gety()));
+
+			for (Position p : neighbours) {
+				if (cost.get(p) == null || cost.get(p) >= cost.get(current) + fma.getTerrainAt(current).cost()
+						+ fma.getTerrainAt(p).cost())
+					cost.put(p, cost.get(current) + fma.getTerrainAt(current).cost() + fma.getTerrainAt(p).cost());
+			}
+
+			current = neighbours.stream().max((Position lhs, Position rhs) -> {
+				return cost.get(lhs) - cost.get(rhs);
+			}).get();
+
+			neighbours.remove(current);
+
+		}
+
+		logger.debug("Dijkstra cost of destination node: " + cost.get(pos));
+
+		// TODO: figure out how to get a path to the destination node and not just the
+		// cost to get there!
 		return null;
 	}
 }
