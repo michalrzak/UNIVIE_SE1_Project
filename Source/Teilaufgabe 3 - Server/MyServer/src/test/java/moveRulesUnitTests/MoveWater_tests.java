031c68d5b77332d22ba282d58a3d6acf68160e62
Michal Robert Žák <a11922222@unet.univie.ac.at>
2021-06-18 09:39:07 +0200

    added a class for move tests. no tests implemented yet

---
+++ Source/Teilaufgabe 3 - Server/MyServer/src/test/java/moveRulesUnitTests/MoveWater_tests.java
@@ -0,0 +1,16 @@
+package moveRulesUnitTests;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+class MoveWater_tests {
+
+	@Test
+	@Disabled
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
